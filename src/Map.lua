62b5e377dde31c4f696841e822a56a0ac533f9b0
averadev <avera@geekbucket.com.mx>
2014-10-02 16:31:29 -0500

    Cambios plantilla cupones

--- src/Map.lua
+++ src/Map.lua
@@ -82,22 +82,34 @@ function scene:createScene( event )
     
     -- Background
     local background = display.newRect(midW, midH, intW , intH)
-    background:setFillColor(0.8, 0.8, 0.8)
+    background:setFillColor(0)
     screenMap:insert(background)
     
+    -- Height status bar
+    local h = display.topStatusBarContentHeight
+    
     -- Creamos toolbar
-    local titleBar = display.newRect( display.contentCenterX, 0, display.contentWidth, 105 )
-    titleBar:setFillColor( titleGradient ) 
-    titleBar.y = display.screenOriginY + titleBar.contentHeight * 0.5
+    local titleBar = display.newRect( display.contentCenterX, h, display.contentWidth, 65 )
+    titleBar.anchorY = 0;
+    titleBar:setFillColor( 0 ) 
     screenMap:insert(titleBar)
+    
+    local lineBar = display.newRect( display.contentCenterX, 63 + h, display.contentWidth, 5 )
+    lineBar:setFillColor( {
+            type = 'gradient',
+            color1 = { 0, .7, 0, 1 }, 
+            color2 = { 0, .7, 0, .5 },
+            direction = "bottom"
+        } ) 
+    screenMap:insert(lineBar)
 
     local btnReturn = display.newImage("img/btn/left.png", true) 
     btnReturn.x = 35
-    btnReturn.y = 70
+    btnReturn.y = 35 + h
     screenMap:insert(btnReturn)
     btnReturn:addEventListener( "tap", gotoMain )
     
-    local title = display.newText( "Lugares cercanos a ti", 250, 72, "Chivo", 22)
+    local title = display.newText( "Lugares cercanos a ti", 250, 35 + h, "Chivo", 22)
     title:setFillColor( .8, .8, .8 )
     screenMap:insert(title)
     
@@ -105,15 +117,18 @@ function scene:createScene( event )
     local sheet = graphics.newImageSheet(Sprites.red.source, Sprites.red.frames)
     sprRed = display.newSprite(sheet, Sprites.red.sequences)
     sprRed.x = intW - 40
-    sprRed.y = 72 
+    sprRed.y = 30 + h 
     screenMap:insert(sprRed)
     
 end
 
 -- Called immediately after scene has moved onscreen:
 function scene:enterScene( event )
+     -- Height status bar
+    local h = display.topStatusBarContentHeight
+    
     -- Cocinar el mapa
-    myMap = native.newMapView( midW, (midH + 53), intW, (intH - 104) )
+    myMap = native.newMapView( midW, (midH + ((65 + h) / 2)), intW, (intH - (65 + h)) )
     myMap:setCenter( 21.154425, -86.820303, 0.02, 0.02 )
     screenMap:insert(myMap)
     
